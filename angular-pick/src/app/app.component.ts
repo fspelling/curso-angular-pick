363983af11b48f15a733ca008d6a050057b5fdcd
Fernando Spelling <fernando.spelling@linx.com.br>
2019-07-17 19:16:15 -0300

    .

---
+++ angular-pick/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Pick';
+  alt = "imagem";
+  url = "../assets/imgs/Capture001.png";
+}
