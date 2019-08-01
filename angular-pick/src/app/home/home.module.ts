e502deb62b3836be4f718be48cb95dad116853fe
Fernando <f.l.spelling@gmail.com>
2019-08-01 00:06:48 -0300

    .

--- angular-pick/src/app/home/home.module.ts
+++ angular-pick/src/app/home/home.module.ts
@@ -1,14 +1,18 @@
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
-import { SignInComponent } from "./signin/signin.component";
+import { SignInComponent } from './signin/signin.component';
+import { VMessageModule } from '../shared/components/vmessage/vmessage.module';
 
 @NgModule({
     declarations: [
         SignInComponent
     ],
     imports: [
-        CommonModule
+        CommonModule,
+        ReactiveFormsModule,
+        VMessageModule
     ]
 })
 export class HomeModule {}
