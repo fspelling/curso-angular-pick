6161307233053240e3465a02b67c6c03b45a357a
Fernando <f.l.spelling@gmail.com>
2019-08-13 01:23:51 -0300

    .

--- angular-pick/src/app/photos/photo/photo.component.ts
+++ angular-pick/src/app/photos/photo/photo.component.ts
@@ -1,10 +1,25 @@
 import { Component, Input } from '@angular/core';
 
+const cloud = 'http://localhost:3000/imgs/';
+
 @Component({
     selector: 'ap-photo',
     templateUrl: './photo.component.html'
 })
 export class PhotoComponent {
+    private _url = '';
+
     @Input() description = '';
-    @Input() url = '';
+
+    @Input() set url(value: string) {
+        if (value && value.startsWith('data')) {
+            this._url = value;
+        } else {
+            this._url = `${cloud}${value}`;
+        }
+    }
+
+    get url() {
+        return this._url;
+    }
 }
