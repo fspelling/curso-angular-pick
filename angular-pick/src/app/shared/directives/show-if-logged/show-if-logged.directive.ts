81fd7a7587270c144fa6f8734e55ed9b82e079ac
Fernando Spelling <fernando.spelling@linx.com.br>
2019-08-30 14:30:25 -0300

    .

--- angular-pick/src/app/shared/directives/show-if-logged/show-if-logged.directive.ts
+++ angular-pick/src/app/shared/directives/show-if-logged/show-if-logged.directive.ts
@@ -6,13 +6,22 @@ import { UserService } from "src/app/core/user/user.service";
     selector: '[showIfLogged]'
 })
 export class ShowIfLoggedDirective implements OnInit {
+    styleDisplay: string;
+
     constructor(
         private userService: UserService,
         private element: ElementRef,
         private renderer: Renderer) { }
 
     ngOnInit(): void {
-        !this.userService.isLogged() && 
-            this.renderer.setElementStyle(this.element.nativeElement, 'display', 'none');
+        this.styleDisplay = getComputedStyle(this.element.nativeElement).display;
+
+        this.userService.getUser()
+            .subscribe(user => {
+                if (user)
+                    this.renderer.setElementStyle(this.element.nativeElement, 'display', this.styleDisplay);
+                else
+                    this.renderer.setElementStyle(this.element.nativeElement, 'display', 'none');
+            });
     }
 }
